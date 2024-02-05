7bc16232fd50568caa90ffac4d6f84138efcd95f
www-author <dream970215@naver.com>
2024-02-05 22:18:34 +0900

    :sparkles: feat : 메인 메서드 추가

---
+++ src/com/shoppingmall/JavaShoppingMallApplication.java
@@ -0,0 +1,18 @@
+package com.shoppingmall;
+
+import java.math.BigDecimal;
+
+public class JavaShoppingMallApplication {
+    public static void main(String[] args) {
+        Product beauty = new Beauty("beauty", 30000, 2);
+        Product grocery = new Grocery("grocery", 20000, 3);
+        Product largeAppliance = new LargeAppliance("largeAppliance", 50000, 5);
+
+        Cart cart = new Cart(
+                new Product[] {beauty, grocery, largeAppliance}
+        );
+
+        BigDecimal totalDeliveryCharge = cart.calculateDeliveryCharge();
+        System.out.println(totalDeliveryCharge);    // 결과: 9000
+    }
+}
